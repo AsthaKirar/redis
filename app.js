78fcfdad5d83f1c491acad7d7218e7d0e650e1ff
AsthaKirar <asthap602@gmail.com>
2024-12-20 22:20:43 +0530

    commit

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const Redis = require('ioredis')
 
 
 
-// const redis = new Redis({
-//     host: "redis-16563.c212.ap-south-1-1.ec2.redns.redis-cloud.com",
-//     port: 16563,
-//     password: "JwTM1KtbvH238eviebLo2cj5xYUrUTTe"
-// })
-
-// redis.on('connect', () => {
-//     console.log("redis connected")
-// })
 app.get('/create-ride', async (req, res) => {
+
+    const redis = new Redis({
+    host: "redis-16563.c212.ap-south-1-1.ec2.redns.redis-cloud.com",
+    port: 16563,
+    password: "JwTM1KtbvH238eviebLo2cj5xYUrUTTe"
+})
+
+redis.on('connect', () => {
+    console.log("redis connected")
+})
 
 const result = await axios.get('http://localhost:3001/distance')
 const data = result.data
